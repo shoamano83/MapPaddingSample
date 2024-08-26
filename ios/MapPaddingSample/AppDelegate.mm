0bbedb1b4e18cbd8b346210ab2b6fbf23233b924
Sho Amano <samano@xevo.com>
2024-08-26 16:27:00 +0900

    Set up react-native-maps for iOS

--- ios/MapPaddingSample/AppDelegate.mm
+++ ios/MapPaddingSample/AppDelegate.mm
@@ -1,11 +1,14 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:[[NSBundle mainBundle] objectForInfoDictionaryKey:@"MAPS_API_KEY"]];
+
   self.moduleName = @"MapPaddingSample";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
